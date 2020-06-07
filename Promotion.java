968ddcc8cda705cd0bef5f88c954da21107a7f6a
enzosrt <63588080+enzosrt@users.noreply.github.com>
2020-06-07 21:59:40 +0200

    promo

---
+++ Promotion.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modele;
+
+/**
+ *
+ * @author ZianL
+ */
+public class Promotion {
+    private int id;
+    private String nom;
+    
+    public Promotion(int id, String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
+    public Promotion(){};
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getNom(){
+        return nom;
+    }
+    
+    public void setNom(String nom){
+        this.nom=nom;
+    }
+}
